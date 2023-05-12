d74079b03fd01bfb294daaea6e10a2075ffb0278
Hafez Ben-Abdillah MADI <hafezbenabdillah.madi@gmail.com>
2023-05-12 23:06:25 +0200

    First implementation of regex parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-mod regex;
+pub mod regex;
 
 use regex::Regex;
 use regex::Symbol;
@@ -6,20 +6,30 @@ use regex::RegexElement;
 use regex::Quantifier;
 
 impl Symbol for &str{}
+impl Symbol for char{}
 
 type StrRegex<'a> = Regex<&'a str>;
+type ChrRegex = Regex<char>;
 
 #[test]
 fn test(){
-    let a = StrRegex::new(RegexElement::Item("i", Quantifier::OneOrMany))
-        .then_elem(
-            RegexElement::Set("10", "20", Quantifier::Exactly(10))
-        ).then(
-            Regex::new(RegexElement::Group(vec![RegexElement::Item("o", Quantifier::Exactly(1))], Quantifier::ZeroOrMany))
-        );
+    let regex = ChrRegex::new()
+        .then(RegexElement::Item('@', Quantifier::ZeroOrOne))
+        .then(RegexElement::Item('>', Quantifier::OneOrMany))
+        .then(RegexElement::Item('i', Quantifier::ZeroOrMany))
+        .then(RegexElement::Item('.', Quantifier::Exactly(1)));
 
-
-    println!("{a:?}");
+    let candidate1 = &"@>i.".chars().collect::<Vec<char>>();
+    let candidate2 = &"@@iii".chars().collect::<Vec<char>>();
+    let candidate3 = &"iii.".chars().collect::<Vec<char>>();
+    let candidate4 = &">>>.".chars().collect::<Vec<char>>();
+    let candidate5 = &">.".chars().collect::<Vec<char>>();
+    
+    assert!(regex.r#match(candidate1) == true);
+    assert!(regex.r#match(candidate2) == false);
+    assert!(regex.r#match(candidate3) == false);
+    assert!(regex.r#match(candidate4) == true);
+    assert!(regex.r#match(candidate5) == true);
 
         
 }
--- src/regex/mod.rs
+++ src/regex/mod.rs
@@ -20,21 +20,74 @@ pub enum RegexElement<T:Symbol>{
 
 #[derive(Debug)]
 pub struct Regex<T:Symbol>{
-    element: RegexElement<T>,
-    next: Option<Box<Regex<T>>>
+    pattern:Vec<RegexElement<T>>
 }
 
 
 impl<T:Symbol> Regex<T>{
-    pub fn new(e: RegexElement<T>) -> Self{ Regex { element: e, next:None } }
+    pub fn new() -> Self{ Regex { pattern: vec![] } }
 
-    pub fn then(mut self, regex:Regex<T>) -> Box<Self>{
-        self.next = Some(Box::new(regex));
-        self.next.unwrap()
+    pub fn then(mut self, e:RegexElement<T>) -> Self{
+        self.pattern.push(e);
+        self
 
     }
 
-    pub fn then_elem(mut self, e: RegexElement<T>) -> Box<Self> { self.then(Regex::new(e)) }
+    fn match_item(value:&T, quantifier:&Quantifier, candidate: &[T]) -> (bool, usize){
+        let mut occurences = 0;
+        for c in candidate{
+            if value == c { occurences+=1 } else{ break; }
+        }
+
+        return (match quantifier{
+            Quantifier::Exactly(qt) => *qt == occurences,
+            Quantifier::OneOrMany => occurences >= 1,
+            Quantifier::ZeroOrMany => occurences >= 0,
+            Quantifier::ZeroOrOne => occurences == 0 || occurences == 1
+        }, occurences)
+
+
+    }
+
+    pub fn r#match(&self, candidate: &[T]) -> bool{
+        let mut pattern_iterator = self.pattern.iter();
+        let mut candidate_iterator = candidate.iter();
+
+        let mut valid = false;
+        let mut symbol = candidate_iterator.next();
+        let mut element = pattern_iterator.next();
+        let mut count = 0;
+
+        loop{
+            match symbol{
+                Some(_) =>{
+                    if let Some(e) = element{
+                        match e {
+                            RegexElement::Item(value, quantifier) => {
+                                let mut passed = 0;
+                                (valid, passed) = Regex::match_item(value, quantifier, candidate);
+                                count += passed;
+
+                                if valid && count == 0{
+                                    element = pattern_iterator.next();
+                                }else if valid && count > 0 {
+                                    symbol =  candidate_iterator.nth(count);
+                                }
+                            },
+                            RegexElement::Set(_, _, _) => { valid = false },
+                            RegexElement::Group(_, _) => { valid = false }
+                        }
+
+                        if !valid { break; }
+
+                        
+                    }
+                },
+                None => break
+            }
+        }
+        valid
+    }
 
     
 }
